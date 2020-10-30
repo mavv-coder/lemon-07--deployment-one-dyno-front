91fa3215f0e3ca2d8901a87ed76c806d5a1fad49
Miguel Ángel Vázquez Vera <vazquezvera.ma@gmail.com>
2020-10-30 16:12:48 +0100

    initial commit

---
+++ src/pods/list/api/list.api.ts
@@ -0,0 +1,16 @@
+import Axios from 'axios';
+import { Member } from './list.api-model';
+
+const url = `${process.env.BASE_API_URL}/members`;
+
+export const getMemberList = async (
+  organization: string
+): Promise<Member[]> => {
+  const { data } = await Axios.get(`${url}/${organization}`, {
+    headers: {
+      'Access-Control-Allow-Origin':
+        'https://mavv-cloud-front-to-back.herokuapp.com',
+    },
+  });
+  return data;
+};
